ae3bf2d99355e95a1476c2365e3bf583bd525bbb
David <david.contage@intelbras.com.br>
2021-08-30 11:48:20 -0300

    change index.js to app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html'); 
 });
@@ -32,6 +34,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(3000, () => {
-  console.log('listening on port 3000');
+server.listen(port, () => {
+  console.log('listening on port 5000');
 });
